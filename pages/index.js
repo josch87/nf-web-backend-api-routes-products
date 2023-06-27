137f0882e1d9d8ed06a1824c64e1b757c77a4e01
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-27 16:36:48 +0200

    homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,11 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js API Routes!</h1>
+      <p>
+        Please see a list of <a href="/products">All products</a>. For your own
+        application you can use our{" "}
+        <a href="/api/products">well documented API</a>.
+      </p>
     </div>
   );
 }
