bb0fcb297688cf5170fe13eaba8ab750f58dfcc2
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-27 16:39:40 +0200

    fix link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js API Routes!</h1>
       <p>
-        Please see a list of <a href="/products">All products</a>. For your own
-        application you can use our{" "}
-        <a href="/api/products">well documented API</a>.
+        Please see a list of <Link href="/products">All products</Link>. For
+        your own application you can use our{" "}
+        <Link href="/api/products">well documented API</Link>.
       </p>
     </div>
   );
