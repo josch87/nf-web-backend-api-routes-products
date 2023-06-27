e6114b01e821b91ecfc16e5857f887e0484e001a
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-27 17:08:10 +0200

    add space

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function HomePage() {
       <h1>Welcome to Next.js API Routes!</h1>
       <p>
         Please see a list of <Link href="/products">all of our products</Link>.
-        For your own application you can use our
+        For your own application you can use our{" "}
         <Link href="/api/products">well documented API</Link>.
       </p>
     </div>
